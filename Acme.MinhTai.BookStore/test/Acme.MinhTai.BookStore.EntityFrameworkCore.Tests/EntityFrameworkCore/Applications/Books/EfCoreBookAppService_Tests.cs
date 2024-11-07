f39ef1a1993db278b6c90b0792712e2a2307b358
mtai0524 <minhnguyen20020524@gmail.com>
2024-11-08 06:34:35 +0700

    p4 run test

---
+++ Acme.MinhTai.BookStore/test/Acme.MinhTai.BookStore.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Books/EfCoreBookAppService_Tests.cs
@@ -0,0 +1,16 @@
+﻿using Acme.MinhTai.BookStore.Books;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Acme.MinhTai.BookStore.EntityFrameworkCore.Applications.Books
+{
+    [Collection(BookStoreTestConsts.CollectionDefinitionName)]
+    public class EfCoreBookAppService_Tests : BookAppService_Tests<BookStoreEntityFrameworkCoreTestModule>
+    {
+
+    }
+}
